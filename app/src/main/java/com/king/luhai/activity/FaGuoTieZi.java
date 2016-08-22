b3dacd30f0e86ec02ed715500497e240d0de5645
ly920525 <710201459@qq.com>
2016-08-22 11:55:44 +0800

    no message

---
+++ app/src/main/java/com/king/luhai/activity/FaGuoTieZi.java
@@ -0,0 +1,17 @@
+package com.king.luhai.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.king.luhai.R;
+
+/**
+ * Created by Administrator on 2016/8/22 0022.
+ */
+public class FaGuoTieZi extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faguotiezi);
+    }
+}
